4b3e99ea545972004feaad2401f4454844360461
DESKTOP-4MVE251\Admin <anhtuan2231999@gmail.com>
2023-10-02 14:48:02 +0700

    no message

--- src/pages/components/layout/Navbar.tsx
+++ src/pages/components/layout/Navbar.tsx
@@ -10,9 +10,10 @@ export default function Navbar() {
     return (
 
         <nav className="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0">
-            <a href="index.html" className="navbar-brand d-flex align-items-center px-4 px-lg-5">
-                <h2 className="m-0 text-primary">WooDY</h2>
-            </a>
+            <Link href="/" className={clsx("navbar-brand d-flex align-items-center px-4 px-lg-5", { "active": router.pathname === "/" })}>
+            <h2 className="m-0 text-primary">WooDY</h2>
+            </Link>
+             
             <button type="button" className={clsx("navbar-toggler me-4", { "collapsed": collapsed })} data-bs-toggle="collapse"
                 data-bs-target="#navbarCollapse" onClick={() => setCollapsed(!collapsed)}>
                 <span className="navbar-toggler-icon"></span>
